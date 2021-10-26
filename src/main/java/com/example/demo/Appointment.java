05eff4c8a0e3eaac30e147d8203fa6f9b740d944
Fabian Andiel <fabian.andiel@yahoo.com>
2021-10-26 12:29:09 +0200

    optimized classes

--- src/main/java/com/example/demo/Appointment.java
+++ src/main/java/com/example/demo/Appointment.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import java.util.Date;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -16,46 +17,47 @@ import javax.persistence.TemporalType;
 @Table(name = "appointments")
 class Appointment {
 
-  private @Id @GeneratedValue Long id;
-  private String title;
-  @Temporal(TemporalType.TIMESTAMP)
-  private Date startTime;
-  @Temporal(TemporalType.TIMESTAMP)
-  private Date endTime;
-  @ManyToOne()
-  @JoinColumn(name = "user_id", nullable = false)
-  private User user;
-
+	  private @Id @GeneratedValue Long id;
+	  private String title;
+	  @Column(unique = true)
+	  @Temporal(TemporalType.TIMESTAMP)
+	  private Date startTime;
+	  @Column(unique = true)
+	  @Temporal(TemporalType.TIMESTAMP)
+	  private Date endTime;
+	  @ManyToOne()
+	  @JoinColumn(name = "user_id", nullable = false)
+	  private User user;
   
-public Appointment(Long id, String title, Date startTime, Date endTime) {
-	this.id = id;
-	this.title = title;
-	this.startTime = startTime;
-	this.endTime = endTime;
-}
-
-public Long getId() {
-	return id;
-}
-
-public String getTitle() {
-	return title;
-}
-public void setTitle(String title) {
-	this.title = title;
-}
-public Date getStartTime() {
-	return startTime;
-}
-public void setStartTime(Date startTime) {
-	this.startTime = startTime;
-}
-public Date getEndTime() {
-	return endTime;
-}
-public void setEndTime(Date endTime) {
-	this.endTime = endTime;
-}
-  
+	public Appointment(String title, Date startTime, Date endTime, User user) {
+		this.title = title;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.user = user;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Date getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+	public Date getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+	  
  
 }
--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -18,44 +18,54 @@ class User {
   private String name;
   @OneToMany(mappedBy = "user", 
           cascade = CascadeType.ALL)
-  private Set<Appointment> pages;
+  private Set<Appointment> appointments;
+  
 
-  User(String name) {
-    this.name = name;
+  public User(String name) {
+	this.name = name;
   }
 
-  public Long getId() {
-    return this.id;
+	public Long getId() {
+		return id;
+	  }
+
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Set<Appointment> getAppointments() {
+		return appointments;
+	}
+	
+	public void setAppointments(Set<Appointment> appointments) {
+		this.appointments = appointments;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(appointments, id, name);
+	}
+  
+  @Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(appointments, other.appointments) && Objects.equals(id, other.id)
+				&& Objects.equals(name, other.name);
   }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-@Override
-public int hashCode() {
-	return Objects.hash(id, name);
-}
-
-@Override
-public boolean equals(Object obj) {
-	if (this == obj)
-		return true;
-	if (obj == null)
-		return false;
-	if (getClass() != obj.getClass())
-		return false;
-	User other = (User) obj;
-	return Objects.equals(id, other.id) && Objects.equals(name, other.name);
-}
-
-@Override
-public String toString() {
-	return "User [id=" + id + ", name=" + name + "]";
-}
-   
+	
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", appointments=" + appointments + "]";
+	}
+    
 }
